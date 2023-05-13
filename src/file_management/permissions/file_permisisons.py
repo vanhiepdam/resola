f2af62153411f3ece227bbdc975038a2b129ac5e
DH <DH>
2023-05-13 15:17:39 +0700

    feat(file-management): create permissions for each api action

---
+++ src/file_management/permissions/file_permisisons.py
@@ -0,0 +1,49 @@
+from rest_framework.permissions import BasePermission
+
+
+class CanRetrieveFilePermission(BasePermission):
+    def has_object_permission(self, request, view, obj) -> bool:  # type: ignore
+        user = request.user
+        if not user.is_authenticated or not user.is_active:
+            return False
+
+        if user.is_superuser:
+            return True
+
+        return user.has_perm("file_management.can_view_file")
+
+
+class CanListFilePermission(BasePermission):
+    def has_permission(self, request, view) -> bool:  # type: ignore
+        user = request.user
+        if not user.is_authenticated or not user.is_active:
+            return False
+
+        if user.is_superuser:
+            return True
+
+        return user.has_perm("file_management.can_view_file")
+
+
+class CanUploadFilePermission(BasePermission):
+    def has_permission(self, request, view) -> bool:  # type: ignore
+        user = request.user
+        if not user.is_authenticated or not user.is_active:
+            return False
+
+        if user.is_superuser:
+            return True
+
+        return user.has_perm("file_management.can_create_file")
+
+
+class CanDeleteFilePermission(BasePermission):
+    def has_permission(self, request, view) -> bool:  # type: ignore
+        user = request.user
+        if not user.is_authenticated or not user.is_active:
+            return False
+
+        if user.is_superuser:
+            return True
+
+        return user.has_perm("file_management.can_delete_file")
