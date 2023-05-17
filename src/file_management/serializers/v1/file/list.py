b922ab36915d419a8bb0fa31875222762113dc86
DH <DH>
2023-05-17 18:59:32 +0700

    chore: Add file_name and presigned_url to list files response

--- src/file_management/serializers/v1/file/list.py
+++ src/file_management/serializers/v1/file/list.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 from file_management.models import File
 from shared.restful.serializers import BaseModelSerializer
 from tenant.models import Resource, Tenant
@@ -25,9 +27,17 @@ class ListFileUserSerializerV1(BaseModelSerializer):
 
 
 class ListFileSerializerV1(BaseModelSerializer):
+    file_name = serializers.SerializerMethodField()
+    presigned_url = serializers.SerializerMethodField()
     resource = ListFileResourceSerializerV1()
     uploaded_by = ListFileUserSerializerV1()
 
     class Meta:
         model = File
-        fields = ["id", "resource", "uploaded_by", "uploaded_at"]
+        fields = ["id", "file_name", "presigned_url", "resource", "uploaded_by", "uploaded_at"]
+
+    def get_file_name(self, obj: File) -> str:
+        return obj.get_original_file_name("file")
+
+    def get_presigned_url(self, obj: File) -> str:
+        return obj.get_file_presigned_url("file", has_read=True)
